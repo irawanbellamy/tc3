662f3eab6df06c1da48ed108ead0576e6f05270d
irawanbellamy <irawanbellamy@gmail.com>
2022-08-31 01:35:15 +0700

    templating

---
+++ application/views/templates/_footer.php
@@ -0,0 +1,87 @@
+<!-- Footer -->
+<footer id="footer">
+	<div class="footer-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="icon-box effect small clean">
+						<div class="icon">
+							<a href="#"><i class="icon-clock"></i></a>
+						</div>
+						<h3>Working Days</h3>
+						<p><strong>Monday - Friday</strong>
+							<br>10:00 AM - 11:00 PM
+						</p>
+						<p><strong>Saturday - Sunday</strong>
+							<br>10:00 AM - 04:00 PM
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="icon-box effect small clean">
+						<div class="icon">
+							<a href="#"><i class="fas fa-map-marker-alt"></i></a>
+						</div>
+						<h3>Caffe Location</h3>
+						<p><strong>Caffe Address:</strong>
+							<br> 795 Folsom Ave, Suite 600
+							<br> San Francisco, CA 94107
+							<br>
+							<br>
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="icon-box effect small clean">
+						<div class="icon">
+							<a href="#"><i class="icon-phone"></i></a>
+						</div>
+						<h3>Caffe Contact</h3>
+						<p><strong>Phone:</strong>
+							<br> (123) 456-7890
+							<br> (987) 654-3210
+							<br>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<!-- Social icons -->
+					<div class="social-icons social-icons-colored float-left">
+						<ul>
+							<li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>
+							<li class="social-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li class="social-twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li class="social-vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>
+							<li class="social-youtube"><a href="#"><i class="fab fa-youtube"></i></a></li>
+							<li class="social-pinterest"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+							<li class="social-gplus"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+						</ul>
+					</div>
+					<!-- end: Social icons -->
+				</div>
+				<div class="col-lg-6">
+					<div class="copyright-text text-center">&copy; <?= date('Y') ?> All Rights Reserved.<a href="<?= base_url() ?>" target="_blank" rel="noopener"> Paguyuban Tenjo City 3</a> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- end: Footer -->
+</div>
+<!-- end: Body Inner -->
+<!-- Scroll top -->
+<a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+<!--Plugins-->
+<script src="<?= base_url() ?>assets/js/jquery.js"></script>
+<script src="<?= base_url() ?>assets/js/plugins.js"></script>
+<!--Template functions-->
+<script src="<?= base_url() ?>assets/js/functions.js"></script>
+</body>
+
+</html>
---
+++ application/views/templates/_header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="tento city" />
+	<meta name="description" content="Tenjo City">
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Document title -->
+	<title>Home</title>
+	<!-- Stylesheets & Fonts -->
+	<link href="<?= base_url() ?>assets/css/plugins.css" rel="stylesheet">
+	<link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+	<!-- Body Inner -->
+	<div class="body-inner">
+		<header id="header" class="light submenu-dark">
+			<div class="header-inner">
+				<div class="container">
+					<!--Logo-->
+					<div id="logo">
+
+						<a href="<?= base_url() ?>">
+							<span class="logo-default">Tenjo City 3</span>
+							<span class="logo-dark">Tenjo City 3</span>
+							<!-- <span class="logo-default"><img src="<?= base_url() ?>assets/images/logos/Tenjo-city.jpg" alt="logo" width="100"></span>
+							<span class="logo-dark"><img src="<?= base_url() ?>assets/images/logos/logo-tenjo-city_copy.png" alt="logo" width="100"></span> -->
+						</a>
+					</div>
+					<!--End: Logo-->
+
+					<!--Navigation Resposnive Trigger-->
+					<div id="mainMenu-trigger">
+						<a class="lines-button x"> <span class="lines"></span> </a>
+					</div>
+					<!--end: Navigation Resposnive Trigger-->
+					<!--Navigation-->
+					<div id="mainMenu" class="menu-center">
+						<div class="container">
+							<nav>
+								<ul>
+									<li><a href="index.html">Tentang Kami</a></li>
+									<li class="dropdown"><a href="#">Paguyuban</a>
+										<ul class="dropdown-menu">
+											<li><a href="widgets.html">Kepengurusan</a></li>
+											<li><a href="widgets.html">AD ART</a></li>
+											<li><a href="widgets.html">Rencana Kegiatan</a></li>
+											<li><a href="widgets.html">Dokumentasi</a></li>
+										</ul>
+									</li>
+									<li><a href="index.html">Gallery</a></li>
+									<li><a href="index.html">Blog</a></li>
+									<li><a href="index.html">Hubungi Kami</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<!--end: Navigation-->
+				</div>
+			</div>
+		</header>
