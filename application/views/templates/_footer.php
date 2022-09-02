1cfa003cb9eff0ee8e557dbcd7d6addf18c85c90
irawanbellamy <irawanbellamy@gmail.com>
2022-09-02 15:12:13 +0700

    fix config

--- application/views/templates/_footer.php
+++ application/views/templates/_footer.php
@@ -81,10 +81,10 @@
 <!-- Scroll top -->
 <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
 <!--Plugins-->
-<script src="js/jquery.js"></script>
-<script src="js/plugins.js"></script>
+<script src="<?= base_url() ?>assets/js/jquery.js"></script>
+<script src="<?= base_url() ?>assets/js/plugins.js"></script>
 <!--Template functions-->
-<script src="js/functions.js"></script>
+<script src="<?= base_url() ?>assets/js/functions.js"></script>
 </body>
 
 </html>
